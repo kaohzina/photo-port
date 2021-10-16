b8626e3b4e98f1f10465f6f2776eab8e871ff65a
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-16 13:10:28 -0500

    standby

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,6 +6,7 @@ function Nav(props) {
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
   }, [currentCategory]);
+  
   const {
     categories = [],
     setCurrentCategory,
