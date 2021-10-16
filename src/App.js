2aa48fc691776508d90ac39279e9b7008d9d22d9
Zinanshin Kaoher <54826833+kaohzina@users.noreply.github.com>
2021-10-16 01:49:04 -0500

    kind of broken on the index nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from "./components/Gallery";
+
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ]);
   return (
     // React.createElement("div", {props}, [array])
     // React.createElement("h1", {props}, "Ginger"),
     // React.createElement("p", {props}, "breed: Brittney Spaniel")
     <div>
-      <Nav></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}>
+      </Nav>
       <main>
+        <div>
+        <Gallery></Gallery>
         <About></About>
+        </div>
       </main>
     </div>
   );
